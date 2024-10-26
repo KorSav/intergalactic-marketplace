0d067be2c430d016a7a2cc37eabe24ef65dde0f3
Savelii Korniiko <saveliykorniko@gmail.com>
2024-10-26 21:32:34 +0300

    initial commit

---
+++ src/main/java/com/example/intergalactic_marketplace/IntergalacticMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.intergalactic_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergalacticMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergalacticMarketplaceApplication.class, args);
+	}
+
+}
