d92cd2ed62ad77412c05b16e73fd4f4d42179120
Savelii Korniiko <saveliykorniko@gmail.com>
2024-10-29 12:20:03 +0200

    added global exception handler

---
+++ src/main/java/com/example/intergalactic_marketplace/web/ExceptionTranslator.java
@@ -0,0 +1,57 @@
+package com.example.intergalactic_marketplace.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.Map;
+import java.net.URI;
+
+import com.example.intergalactic_marketplace.service.exception.ProductAlreadyExistsException;
+import com.example.intergalactic_marketplace.service.exception.ProductNotFoundException;
+import com.example.intergalactic_marketplace.service.exception.ProductsNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionTranslator extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    ProblemDetail handleProductNotFoundException(ProductNotFoundException ex, WebRequest request) {
+        log.info("Product not found exception raised");
+        String path = request.getDescription(false).replace("uri=", "");
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setProperties(Map.of(
+            "error", URI.create("product-not-found"),
+            "path", path
+        ));
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ProductsNotFoundException.class)
+    ProblemDetail handleProductsNotFoundException(ProductsNotFoundException ex, WebRequest request) {
+        log.info("Products not found exception raised");
+        String path = request.getDescription(false).replace("uri=", "");
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setProperties(Map.of(
+            "error", URI.create("products-not-found"),
+            "path", path
+        ));
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    ProblemDetail handleProductAlreadyExistsException(ProductAlreadyExistsException ex, WebRequest request) {
+        log.info("Product already exists exception raised");
+        String path = request.getDescription(false).replace("uri=", "");
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        problemDetail.setProperties(Map.of(
+            "error", URI.create("product-already-exists"),
+            "path", path
+        ));
+        return problemDetail;
+    }
+}
