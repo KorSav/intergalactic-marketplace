c2bc5a03c03a688afedf3dd4a44defcdc0a02232
Savelii Korniiko <saveliykorniko@gmail.com>
2024-10-29 13:23:50 +0200

    added builder and getters

--- src/main/java/com/example/intergalactic_marketplace/domain/Customer.java
+++ src/main/java/com/example/intergalactic_marketplace/domain/Customer.java
@@ -1,5 +1,10 @@
 package com.example.intergalactic_marketplace.domain;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
 public class Customer {
     Long id;
     String name;
