753da27df5e6846b2d70bd7b6ed8e9df00151ce9
Savelii Korniiko <saveliykorniko@gmail.com>
2024-10-29 12:18:48 +0200

    added product exceptions

---
+++ src/main/java/com/example/intergalactic_marketplace/service/exception/ProductAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.example.intergalactic_marketplace.service.exception;
+
+public class ProductAlreadyExistsException extends RuntimeException {
+private static final String baseMessage = "Product with name {} already exists";
+
+    public ProductAlreadyExistsException(String name){
+        super(String.format(baseMessage, name));
+    }
+}
+
