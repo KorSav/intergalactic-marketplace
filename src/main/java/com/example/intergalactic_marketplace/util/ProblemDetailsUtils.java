dea212e69c5c7931bd0c65acc75564fbc314688d
Savelii Korniiko <saveliykorniko@gmail.com>
2024-11-01 20:58:49 +0200

    fixed error responses

--- src/main/java/com/example/intergalactic_marketplace/util/ProblemDetailsUtils.java
+++ src/main/java/com/example/intergalactic_marketplace/util/ProblemDetailsUtils.java
@@ -1,23 +1,36 @@
 package com.example.intergalactic_marketplace.util;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-
 import com.example.intergalactic_marketplace.web.exception.ParamsViolationDetails;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
-import java.net.URI;
 import lombok.experimental.UtilityClass;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
 
 @UtilityClass
 public class ProblemDetailsUtils {
-    public ProblemDetail getValidationErrorsProblemDetail(List<ParamsViolationDetails> validationResponse, String path){
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("validation-error"),
-            "path", path,
-            "invalidParams", validationResponse
-        ));
-        return problemDetail;
-    }
+  public Map<String, Object> getValidationErrorsBody(
+      List<ParamsViolationDetails> validationResponse, String path) {
+    Map<String, Object> response =
+        Map.of(
+            "status",
+            HttpStatus.BAD_REQUEST,
+            "error",
+            URI.create("validation-error"),
+            "path",
+            path,
+            "invalidParams",
+            validationResponse);
+    return response;
+  }
+
+  public ResponseEntity<Map<String, Object>> getErrorResponseEntity(
+      HttpStatus status, String errorType, WebRequest request, String message) {
+    String path = request.getDescription(false).replace("uri=", "");
+    Map<String, Object> response =
+        Map.of("status", status.value(), "error", errorType, "path", path, "message", message);
+    return ResponseEntity.status(status).body(response);
+  }
 }
--- src/main/java/com/example/intergalactic_marketplace/web/ExceptionTranslator.java
+++ src/main/java/com/example/intergalactic_marketplace/web/ExceptionTranslator.java
@@ -1,9 +1,19 @@
 package com.example.intergalactic_marketplace.web;
 
+import com.example.intergalactic_marketplace.service.exception.CustomerHasNoRulesToDeleteProductException;
+import com.example.intergalactic_marketplace.service.exception.CustomerHasNoRulesToUpdateProductException;
+import com.example.intergalactic_marketplace.service.exception.CustomerNotFoundException;
+import com.example.intergalactic_marketplace.service.exception.ProductAlreadyExistsException;
+import com.example.intergalactic_marketplace.service.exception.ProductNotFoundException;
+import com.example.intergalactic_marketplace.service.exception.ProductsNotFoundException;
+import com.example.intergalactic_marketplace.util.ProblemDetailsUtils;
+import com.example.intergalactic_marketplace.web.exception.ParamsViolationDetails;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,112 +22,77 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.List;
-import java.util.Map;
-import java.net.URI;
-
-import com.example.intergalactic_marketplace.service.exception.CustomerHasNoRulesToDeleteProductException;
-import com.example.intergalactic_marketplace.service.exception.CustomerHasNoRulesToUpdateProductException;
-import com.example.intergalactic_marketplace.service.exception.CustomerNotFoundException;
-import com.example.intergalactic_marketplace.service.exception.ProductAlreadyExistsException;
-import com.example.intergalactic_marketplace.service.exception.ProductNotFoundException;
-import com.example.intergalactic_marketplace.service.exception.ProductsNotFoundException;
-import com.example.intergalactic_marketplace.web.exception.ParamsViolationDetails;
-import com.example.intergalactic_marketplace.util.ProblemDetailsUtils;
-
-import lombok.extern.slf4j.Slf4j;
-
 @ControllerAdvice
 @Slf4j
 public class ExceptionTranslator extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    ProblemDetail handleProductNotFoundException(ProductNotFoundException ex, WebRequest request) {
-        log.info("Product not found exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("product-not-found"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(ProductNotFoundException.class)
+  ResponseEntity<Map<String, Object>> handleProductNotFoundException(
+      ProductNotFoundException ex, WebRequest request) {
+    log.info("Product not found exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.NOT_FOUND, "product-not-found", request, ex.getMessage());
+  }
 
-    @ExceptionHandler(ProductsNotFoundException.class)
-    ProblemDetail handleProductsNotFoundException(ProductsNotFoundException ex, WebRequest request) {
-        log.info("Products not found exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("products-not-found"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(ProductsNotFoundException.class)
+  ResponseEntity<Map<String, Object>> handleProductsNotFoundException(
+      ProductsNotFoundException ex, WebRequest request) {
+    log.info("Products not found exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.NOT_FOUND, "products-not-found", request, ex.getMessage());
+  }
 
-    @ExceptionHandler(ProductAlreadyExistsException.class)
-    ProblemDetail handleProductAlreadyExistsException(ProductAlreadyExistsException ex, WebRequest request) {
-        log.info("Product already exists exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("product-already-exists"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(ProductAlreadyExistsException.class)
+  ResponseEntity<Map<String, Object>> handleProductAlreadyExistsException(
+      ProductAlreadyExistsException ex, WebRequest request) {
+    log.info("Product already exists exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.CONFLICT, "product-already-exists", request, ex.getMessage());
+  }
 
-    @ExceptionHandler(CustomerHasNoRulesToDeleteProductException.class)
-    ProblemDetail handleCustomerHasNoRulesToDeleteProduct(CustomerHasNoRulesToDeleteProductException ex, WebRequest request) {
-        log.info("Customer has no rules to delete product exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("customer-has-no-rules"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(CustomerHasNoRulesToDeleteProductException.class)
+  ResponseEntity<Map<String, Object>> handleCustomerHasNoRulesToDeleteProduct(
+      CustomerHasNoRulesToDeleteProductException ex, WebRequest request) {
+    log.info("Customer has no rules to delete product exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.FORBIDDEN, "customer-has-no-rules", request, ex.getMessage());
+  }
 
-    @ExceptionHandler(CustomerNotFoundException.class)
-    ProblemDetail handleCustomerNotFoundException(CustomerNotFoundException ex, WebRequest request) {
-        log.info("Customer not found exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("customer-not-found"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(CustomerNotFoundException.class)
+  ResponseEntity<Map<String, Object>> handleCustomerNotFoundException(
+      CustomerNotFoundException ex, WebRequest request) {
+    log.info("Customer not found exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.NOT_FOUND, "customer-not-found", request, ex.getMessage());
+  }
 
-    @ExceptionHandler(CustomerHasNoRulesToUpdateProductException.class)
-    ProblemDetail handleCustomerHasNoRulesToUpdateProductException(CustomerHasNoRulesToUpdateProductException ex, WebRequest request) {
-        log.info("Customer has no rules to update product exception raised");
-        String path = request.getDescription(false).replace("uri=", "");
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
-        problemDetail.setProperties(Map.of(
-            "error", URI.create("customer-has-no-rules"),
-            "path", path,
-            "message", ex.getMessage()
-        ));
-        return problemDetail;
-    }
+  @ExceptionHandler(CustomerHasNoRulesToUpdateProductException.class)
+  ResponseEntity<Map<String, Object>> handleCustomerHasNoRulesToUpdateProductException(
+      CustomerHasNoRulesToUpdateProductException ex, WebRequest request) {
+    log.info("Customer has no rules to update product exception raised");
+    return ProblemDetailsUtils.getErrorResponseEntity(
+        HttpStatus.FORBIDDEN, "customer-has-no-rules", request, ex.getMessage());
+  }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status,
-        WebRequest request) {
-        log.info("Input params validation failed");
-        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
-        List<ParamsViolationDetails> validationResponse =
-            errors.stream().map(err -> ParamsViolationDetails.builder().reason(err.getDefaultMessage()).fieldName(err.getField()).build()).toList();
-        String path = request.getDescription(false).replace("uri=", "");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ProblemDetailsUtils.getValidationErrorsProblemDetail(validationResponse, path));
-    }
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    log.info("Input params validation failed");
+    List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+    List<ParamsViolationDetails> validationResponse =
+        errors.stream()
+            .map(
+                err ->
+                    ParamsViolationDetails.builder()
+                        .reason(err.getDefaultMessage())
+                        .fieldName(err.getField())
+                        .build())
+            .toList();
+    String path = request.getDescription(false).replace("uri=", "");
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ProblemDetailsUtils.getValidationErrorsBody(validationResponse, path));
+  }
 }
