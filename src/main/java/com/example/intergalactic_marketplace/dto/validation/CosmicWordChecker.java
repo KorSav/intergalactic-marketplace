65fef4cd3e1a1950fc035eac6a6a3aa3bcecf4f9
Savelii Korniiko <saveliykorniko@gmail.com>
2024-11-01 18:59:23 +0200

    fixed lower case value

--- src/main/java/com/example/intergalactic_marketplace/dto/validation/CosmicWordChecker.java
+++ src/main/java/com/example/intergalactic_marketplace/dto/validation/CosmicWordChecker.java
@@ -5,17 +5,12 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.List;
 
 public class CosmicWordChecker implements ConstraintValidator<CosmicWordCheck, String> {
-    
-    private final List<String> cosmicWords = List.of(
-        "star", "galaxy", "comet"
-    );
 
-    @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        String lowerCaseValue = value;
-        return cosmicWords.stream()
-            .map(String::toLowerCase)
-            .anyMatch(lowerCaseValue::contains);
-    }
-    
+  private final List<String> cosmicWords = List.of("star", "galaxy", "comet");
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    String lowerCaseValue = value.toLowerCase();
+    return cosmicWords.stream().map(String::toLowerCase).anyMatch(lowerCaseValue::contains);
+  }
 }
